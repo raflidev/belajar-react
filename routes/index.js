13f2e754890f70d0e8b9171e027bcd8a40c649c9
Rafli Ramadhan <rafliramdhn@gmail.com>
2022-04-03 22:45:11 +0700

    selesai buat backend login system

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {getUsers, Register, Login} from '../controllers/Users.js'
+import {getUsers, Register, Login, Logout} from '../controllers/Users.js'
 import { verifyToken } from '../middleware/verifyToken.js';
 import { refreshToken } from '../controllers/RefreshToken.js'
 const router = express.Router();
@@ -8,5 +8,6 @@ router.get('/users', verifyToken, getUsers);
 router.post('/users', Register);
 router.post('/login', Login);
 router.get('/token', refreshToken);
+router.delete('/logout', Logout);
 
 export default router;

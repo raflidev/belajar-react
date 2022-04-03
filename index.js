a437ba2d58dcd1f26217c91566d43b26f64fb61f
Rafli Ramadhan <rafliramdhn@gmail.com>
2022-04-03 15:49:21 +0700

    refreshtoken

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import db from "./config/Database.js"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
+import router from "./routes/index.js"
+
+dotenv.config();
+const app = express();
+
+try {
+  await db.authenticate()
+  console.log("Database connected..");
+} catch (error) {
+  console.error(error);
+}
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(router);
+
+app.listen(5000, () => {
+  console.log("Server Running on port 5000");
+})
